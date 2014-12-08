1adbd025f19d6011a8ebde1ca1630f2c11dcdaf4
Zhao Zhenlong <zzl_1164@126.com>
2014-12-08 21:20:58 +0800

    map user read-only UPAGES

--- kern/pmap.c
+++ kern/pmap.c
@@ -171,6 +171,8 @@ mem_init(void)
 	//    - pages itself -- kernel RW, user NONE
 	// Your code goes here:
 
+	boot_map_region(kern_pgdir, UPAGES, ROUNDUP(npages*sizeof(struct PageInfo), PGSIZE), PADDR(pages), PTE_U);
+
 	//////////////////////////////////////////////////////////////////////
 	// Use the physical memory that 'bootstack' refers to as the kernel
 	// stack.  The kernel stack grows down from virtual address KSTACKTOP.
