5273837680c4c39869b30bfad068eedebc66809c
Zhao Zhenlong <zzl_1164@126.com>
2015-03-02 01:09:40 +0800

    initialize transmit descriptors (exercise 5)

--- kern/e1000.c
+++ kern/e1000.c
@@ -7,8 +7,32 @@
 #include <kern/e1000.h>
 
 // LAB 6: Your driver code here
+#define TX_RING_SIZE 32		// should be multiple of 8 because of align
 
 volatile uint32_t *e1000_va;
+__attribute__((__aligned__(16)))
+struct tx_desc tx_ring[TX_RING_SIZE];
+
+int e1000_tx_pkt(void* data, int len);
+
+static uint32_t e1000_cfg_get(uint32_t off) {
+	return e1000_va[off >> 2];
+}
+
+static void e1000_cfg_set(uint32_t off, uint32_t data) {
+	e1000_va[off >> 2] = data;
+}
+
+void e1000_init_transmit(void) {
+	memset(tx_ring, 0, sizeof(struct tx_desc) * TX_RING_SIZE);
+	e1000_cfg_set(E1000_TDBAL, PADDR(&tx_ring[0]));
+	e1000_cfg_set(E1000_TDBAH, 0);
+	e1000_cfg_set(E1000_TDLEN, TX_RING_SIZE << 4);
+	e1000_cfg_set(E1000_TDH, 0);
+	e1000_cfg_set(E1000_TDT, 0);
+	e1000_cfg_set(E1000_TCTL, E1000_TCTL_EN | E1000_TCTL_PSP| 0x40 << 12);
+	e1000_cfg_set(E1000_TIPG, 0x0060080a);
+}
 
 int pci_e1000_attach(struct pci_func *f)
 {
@@ -51,6 +75,7 @@ int pci_e1000_attach(struct pci_func *f)
 		f->reg_size[regnum] = size;
 
 		e1000_va = mmio_map_region(base, size);
+		e1000_init_transmit();
 		cprintf("E1000 %02x:%02x.%d (%04x:%04x) enabled\n",
 			f->bus->busno, f->dev, f->func,
 			PCI_VENDOR(f->dev_id), PCI_PRODUCT(f->dev_id));
--- kern/e1000.h
+++ kern/e1000.h
@@ -1,9 +1,96 @@
 #ifndef JOS_KERN_E1000_H
 #define JOS_KERN_E1000_H
 
+#include <inc/types.h>
 #define PCI_DEVICE_ID_E1000_82540EM	0x100e
 #define PCI_VENDOR_ID_INTEL		0x8086
 
+/* Register Set. (82543, 82544)
+ *
+ * Registers are defined to be 32 bits and  should be accessed as 32 bit values.
+ * These registers are physically located on the NIC, but are mapped into the
+ * host memory address space.
+ *
+ * RW - register is both readable and writable
+ * RO - register is read only
+ * WO - register is write only
+ * R/clr - register is read only and is cleared when read
+ * A - register array
+ */
+#define E1000_STATUS   0x00008  /* Device Status - RO */
+#define E1000_TCTL     0x00400  /* TX Control - RW */
+#define E1000_TIPG     0x00410  /* TX Inter-packet gap -RW */
+
+#define E1000_TDFH     0x03410  /* TX Data FIFO Head - RW */
+#define E1000_TDFT     0x03418  /* TX Data FIFO Tail - RW */
+#define E1000_TDFHS    0x03420  /* TX Data FIFO Head Saved - RW */
+#define E1000_TDFTS    0x03428  /* TX Data FIFO Tail Saved - RW */
+#define E1000_TDFPC    0x03430  /* TX Data FIFO Packet Count - RW */
+#define E1000_TDBAL    0x03800  /* TX Descriptor Base Address Low - RW */
+#define E1000_TDBAH    0x03804  /* TX Descriptor Base Address High - RW */
+#define E1000_TDLEN    0x03808  /* TX Descriptor Length - RW */
+#define E1000_TDH      0x03810  /* TX Descriptor Head - RW */
+#define E1000_TDT      0x03818  /* TX Descripotr Tail - RW */
+#define E1000_TIDV     0x03820  /* TX Interrupt Delay Value - RW */
+#define E1000_TXDCTL   0x03828  /* TX Descriptor Control - RW */
+#define E1000_TADV     0x0382C  /* TX Interrupt Absolute Delay Val - RW */
+#define E1000_TSPMT    0x03830  /* TCP Segmentation PAD & Min Threshold - RW */
+#define E1000_TARC0    0x03840  /* TX Arbitration Count (0) */
+#define E1000_TDBAL1   0x03900  /* TX Desc Base Address Low (1) - RW */
+#define E1000_TDBAH1   0x03904  /* TX Desc Base Address High (1) - RW */
+#define E1000_TDLEN1   0x03908  /* TX Desc Length (1) - RW */
+#define E1000_TDH1     0x03910  /* TX Desc Head (1) - RW */
+#define E1000_TDT1     0x03918  /* TX Desc Tail (1) - RW */
+#define E1000_TXDCTL1  0x03928  /* TX Descriptor Control (1) - RW */
+#define E1000_TARC1    0x03940  /* TX Arbitration Count (1) */
+
+/* Transmit Descriptor bit definitions */
+#define E1000_TXD_DTYP_D     0x00100000 /* Data Descriptor */
+#define E1000_TXD_DTYP_C     0x00000000 /* Context Descriptor */
+#define E1000_TXD_POPTS_IXSM 0x01       /* Insert IP checksum */
+#define E1000_TXD_POPTS_TXSM 0x02       /* Insert TCP/UDP checksum */
+#define E1000_TXD_CMD_EOP    0x01000000 /* End of Packet */
+#define E1000_TXD_CMD_IFCS   0x02000000 /* Insert FCS (Ethernet CRC) */
+#define E1000_TXD_CMD_IC     0x04000000 /* Insert Checksum */
+#define E1000_TXD_CMD_RS     0x08000000 /* Report Status */
+#define E1000_TXD_CMD_RPS    0x10000000 /* Report Packet Sent */
+#define E1000_TXD_CMD_DEXT   0x20000000 /* Descriptor extension (0 = legacy) */
+#define E1000_TXD_CMD_VLE    0x40000000 /* Add VLAN tag */
+#define E1000_TXD_CMD_IDE    0x80000000 /* Enable Tidv register */
+#define E1000_TXD_STAT_DD    0x00000001 /* Descriptor Done */
+#define E1000_TXD_STAT_EC    0x00000002 /* Excess Collisions */
+#define E1000_TXD_STAT_LC    0x00000004 /* Late Collisions */
+#define E1000_TXD_STAT_TU    0x00000008 /* Transmit underrun */
+#define E1000_TXD_CMD_TCP    0x01000000 /* TCP packet */
+#define E1000_TXD_CMD_IP     0x02000000 /* IP packet */
+#define E1000_TXD_CMD_TSE    0x04000000 /* TCP Seg enable */
+#define E1000_TXD_STAT_TC    0x00000004 /* Tx Underrun */
+
+/* Transmit Control */
+#define E1000_TCTL_RST    0x00000001    /* software reset */
+#define E1000_TCTL_EN     0x00000002    /* enable tx */
+#define E1000_TCTL_BCE    0x00000004    /* busy check enable */
+#define E1000_TCTL_PSP    0x00000008    /* pad short packets */
+#define E1000_TCTL_CT     0x00000ff0    /* collision threshold */
+#define E1000_TCTL_COLD   0x003ff000    /* collision distance */
+#define E1000_TCTL_SWXOFF 0x00400000    /* SW Xoff transmission */
+#define E1000_TCTL_PBE    0x00800000    /* Packet Burst Enable */
+#define E1000_TCTL_RTLC   0x01000000    /* Re-transmit on late collision */
+#define E1000_TCTL_NRTU   0x02000000    /* No Re-transmit on underrun */
+#define E1000_TCTL_MULR   0x10000000    /* Multiple request support */
+
+struct tx_desc
+{
+	uint64_t addr;
+	uint16_t length;
+	uint8_t cso;
+	uint8_t cmd;
+	uint8_t status;
+	uint8_t css;
+	uint16_t special;
+};
+
 int pci_e1000_attach(struct pci_func *f);
+void e1000_init_transmit(void);
 
 #endif	// JOS_KERN_E1000_H
