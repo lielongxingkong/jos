832b13b3d0df52e1a207bcf2b6d09d225e9540eb
Zhao Zhenlong <zzl_1164@126.com>
2015-03-02 15:10:56 +0800

    implement output help for transmiting packets (exercise 8)

--- net/output.c
+++ net/output.c
@@ -1,5 +1,6 @@
 #include "ns.h"
 
+int debug = 0;
 extern union Nsipc nsipcbuf;
 
 void
@@ -10,4 +11,19 @@ output(envid_t ns_envid)
 	// LAB 6: Your code here:
 	// 	- read a packet from the network server
 	//	- send the packet to the device driver
+
+	int whom, perm, r;
+	struct jif_pkt *pkt;
+
+	while(1) {
+		r = ipc_recv((int32_t *) &whom, &nsipcbuf, &perm);
+		pkt = &nsipcbuf.pkt;
+		if (debug)
+			cprintf("ns req %d from %08x [pkt %08x: %s]\n",
+				r, whom, pkt->jp_len, pkt->jp_data);
+		if (r != NSREQ_OUTPUT)
+			continue;
+		r = sys_transmit(pkt->jp_data, pkt->jp_len);
+	}
+
 }
